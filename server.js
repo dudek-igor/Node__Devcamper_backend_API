3b1a557019d5fbe6f4ac1cf7ac10c27a47ce4d66
Igor Dudek <dudekigor@gmail.com>
2020-10-27 20:59:54 +0100

    Added Router

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars from config
 dotenv.config({ path: './config/config.env' });
-
+// Initial server
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+//Added Listening
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server listening on ${PORT}, mode ${process.env.NODE_ENV}`)
