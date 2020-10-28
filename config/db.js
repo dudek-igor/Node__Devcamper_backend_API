12d0fbc0a6f8e637143cc077d5101968f490bfa1
Igor Dudek <dudekigor@gmail.com>
2020-10-28 11:43:40 +0100

    Added MongoDB

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const db = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected: ${db.connection.host}`.cyan.underline.bold);
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load env vars from config
+dotenv.config({ path: './config/config.env' });
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-// Load env vars from config
-dotenv.config({ path: './config/config.env' });
+//Connect with Database
+connectDB();
 // Initial server
 const app = express();
 
@@ -20,7 +25,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 //Added Listening
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server listening on ${PORT}, mode ${process.env.NODE_ENV}`)
+  console.log(
+    `Server listening on ${PORT}, mode ${process.env.NODE_ENV}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
