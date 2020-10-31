61ead94a7c8314d5d87ec3e6fc2754043ea97cf9
Igor Dudek <dudekigor@gmail.com>
2020-10-31 14:16:01 +0100

    Added Aggregate Functionality

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   const db = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
   });
   console.log(`MongoDB Connected: ${db.connection.host}`.cyan.underline.bold);
